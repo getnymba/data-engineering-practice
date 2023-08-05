8b417c3acc68adc00cd7177e13dcbe0a17a364ab
Nyambayar Demberelsuren <59921058+getnymba@users.noreply.github.com>
2023-08-05 16:04:22 +0800

    ex1, ex2

--- Exercises/Exercise-1/main.py
+++ Exercises/Exercise-1/main.py
@@ -1,5 +1,9 @@
 import requests
-
+import zipfile
+import os
+import aiohttp        
+import aiofiles
+import asyncio
 download_uris = [
     "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2018_Q4.zip",
     "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2019_Q1.zip",
@@ -11,10 +15,26 @@ download_uris = [
 ]
 
 
-def main():
+async def main():
     # your code here
-    pass
+    if not os.path.isdir("./downloads"):
+        os.makedirs("./downloads")
+    async with aiohttp.ClientSession() as session:
+        for url in download_uris:
+            local_filename = url.split('/')[-1]
+            try:
+                async with session.get(url) as resp:
+                    if resp.status == 200:
+                        f = await aiofiles.open(f'./downloads/{local_filename}', mode='wb')
+                        await f.write(await resp.read())
+                        await f.close()
+                        with zipfile.ZipFile(f'./downloads/{local_filename}', 'r') as zip_ref:
+                            zip_ref.extractall('./downloads/')
+                        os.remove(f"./downloads/{local_filename}")
+                        print(f"successfully downloaded {local_filename}")
+            except:
+                print(f"failed to download {local_filename}")
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
--- Exercises/Exercise-2/main.py
+++ Exercises/Exercise-2/main.py
@@ -1,11 +1,49 @@
 import requests
-import pandas
+import pandas as pd
+from bs4 import BeautifulSoup
+import urllib.request
+import re
+import zipfile
+import os
+import aiohttp        
+import aiofiles
+import asyncio
+import glob
 
-
-def main():
-    # your code here
-    pass
-
-
+async def main():
+    contents = requests.get('https://www.ncei.noaa.gov/data/local-climatological-data/access/2021/')
+    contents_soup = BeautifulSoup(contents.text, 'html.parser')
+    contents_tr = contents_soup.find_all('tr')
+    download_uris = []
+    for tr in contents_tr:
+        if "2022-02-07 14:03" in str(tr):
+            file_name = re.search(r'href="(.*?)"', str(tr)).group(1)
+            download_uris.append(f"https://www.ncei.noaa.gov/data/local-climatological-data/access/2021/{file_name}")
+    if not os.path.isdir("./downloads"):
+        os.makedirs("./downloads")
+        
+    async with aiohttp.ClientSession() as session:
+        for url in download_uris:
+            local_filename = url.split('/')[-1]
+            if not os.path.isfile(f"./downloads/{local_filename}"):
+                try:
+                    async with session.get(url) as resp:
+                        if resp.status == 200:
+                            f = await aiofiles.open(f'./downloads/{local_filename}', mode='wb')
+                            await f.write(await resp.read())
+                            await f.close()
+                            print(f"successfully downloaded {local_filename}")
+                except:
+                    print(f"failed to download {local_filename}")
+    all_files = glob.glob(os.path.join('./downloads', "*.csv"))
+    records = []
+    for file_path in all_files:
+        try:
+            df = pd.read_csv(file_path, low_memory=False)
+            records.append(int(df['HourlyDryBulbTemperature'].max()))
+        except:
+            # print(f"failed to extract value from {file_path.split('/')[-1]}")
+            pass
+    print("Highest HourlyDryBulbTemperature is", max(records))
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
